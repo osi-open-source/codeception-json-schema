0b6707c6256ebf838c84702e7acff01f446fad5c
Ivelin Pavlov <ibpavlov@users.noreply.github.com>
2019-06-07 23:04:27 +0300

    Update JsonSchema.php

--- src/Codeception/Module/JsonSchema.php
+++ src/Codeception/Module/JsonSchema.php
@@ -10,14 +10,19 @@ use JsonSchema\Validator;
  */
 class JsonSchema extends Module
 {
-
     /**
      * Validate response by json schema
-     * @param string $schemaRef object or json string
+     * @param string|object|array|null $schemaRef object or json string
+     * @return void
      */
-    public function seeResponseIsValidOnSchema($schemaRef)
+    public function seeResponseIsValidOnSchema($schemaRef, $response = null): void
     {
-        $response = $this->getModule('REST')->response;
+        if(!isset($response)) {
+            $response = $this->getModule('REST')->grabResponse();
+        }
+        if(is_array($response) || is_object($response)) {
+            $response = json_encode($response);
+        }
 
         $validator = new Validator();
         $decodedResponse = json_decode($response);
@@ -26,6 +31,7 @@ class JsonSchema extends Module
         $message = '';
         $isValid = $validator->isValid();
         if (!$isValid) {
+            $this->debug($response);
             $message = 'JSON does not validate. Violations:' . PHP_EOL;
             foreach ($validator->getErrors() as $error) {
                 $message .= $error['property'] . ' ' . $error['message'] . PHP_EOL;
@@ -39,9 +45,20 @@ class JsonSchema extends Module
      * Validate response by json schema
      * @param string $schema path to json schema file
      */
-    public function seeResponseIsValidOnSchemaFile($schema)
+    public function seeResponseIsValidOnSchemaFile(string $schema)
     {
         $schemaRef = (object)['$ref' => 'file://' . realpath($schema)];
         $this->seeResponseIsValidOnSchema($schemaRef);
+    }
+
+    /**
+     * Validate response by json schema
+     * @param string|array|null $json json data
+     * @param string $schema path to json schema file
+     */
+    public function seeJsonValidOnSchemaFile($json, string $schema)
+    {
+        $schemaRef = (object)['$ref' => 'file://' . realpath($schema)];
+        $this->seeResponseIsValidOnSchema($schemaRef, $json);
     }
 }
